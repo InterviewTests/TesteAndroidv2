83a9aebfa87bc2f394f6513f1dd02aae9f577c1a
Ygor César <ygorcross@gmail.com>
2018-11-02 11:07:51 -0300

    Initial commit

---
+++ app/src/main/java/com/testeandroidv2/ygorcesar/testeandroidv2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.testeandroidv2.ygorcesar.testeandroidv2
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
