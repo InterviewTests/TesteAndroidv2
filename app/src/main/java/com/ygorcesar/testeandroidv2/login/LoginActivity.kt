bd62e4f8974b91ef5e317b29a8eb9c0addb5c4d2
Ygor César <ygorcross@gmail.com>
2018-11-02 14:28:57 -0300

    Created layouts, styles, colors, dimens and assets of res folder

---
+++ app/src/main/java/com/ygorcesar/testeandroidv2/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.ygorcesar.testeandroidv2.login
+
+import com.ygorcesar.testeandroidv2.R
+import com.ygorcesar.testeandroidv2.base.presentation.BaseActivity
+
+class LoginActivity : BaseActivity() {
+
+    override val layoutResId = R.layout.login_activity
+
+    override fun onInit() {
+
+    }
+}
