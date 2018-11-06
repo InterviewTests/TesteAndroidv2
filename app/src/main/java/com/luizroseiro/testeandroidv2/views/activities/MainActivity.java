7af1ee179290e549530d7dfd375ed457866cbd57
Luiz Roseiro <luiz.roseiro@gmail.com>
2018-11-06 00:41:46 -0200

    Initial commit with project initial structure

---
+++ app/src/main/java/com/luizroseiro/testeandroidv2/views/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.luizroseiro.testeandroidv2.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.luizroseiro.testeandroidv2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
