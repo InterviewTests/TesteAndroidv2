f49849e96500f445d334caa50ef1eb54ad943d85
PaulimJr <paulim.jr@hotmail.com>
2018-11-10 16:12:00 -0200

    Configuration URl of project and creating the Splash screen

--- app/src/main/java/br/com/ibm/teste/android/ui/activities/SplashActivity.kt
+++ app/src/main/java/br/com/ibm/teste/android/ui/activities/SplashActivity.kt
@@ -1,7 +1,8 @@
-package br.com.ibm.teste.android
+package br.com.ibm.teste.android.ui.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.ibm.teste.android.R
 
 class SplashActivity : AppCompatActivity() {
 
