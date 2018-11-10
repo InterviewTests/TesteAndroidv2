0ae2a118d283a60b60a0d224c4dc5c7b0a445753
PaulimJr <paulim.jr@hotmail.com>
2018-11-10 15:36:52 -0200

    Initial commit with architecture

---
+++ app/src/main/java/br/com/ibm/teste/android/SplashActivity.kt
@@ -0,0 +1,12 @@
+package br.com.ibm.teste.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_splash)
+    }
+}
