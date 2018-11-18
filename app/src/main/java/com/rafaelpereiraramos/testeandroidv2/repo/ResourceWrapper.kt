19afed89e62d5042f43c5600ccc3606941480c1f
Rafael R <raha148@hotmail.com>
2018-11-18 15:53:14 -0200

    Wrapper de request a camada Repository, com Status de carregamento, valor esperado e possíveis mensagem de erro.

---
+++ app/src/main/java/com/rafaelpereiraramos/testeandroidv2/repo/ResourceWrapper.kt
@@ -0,0 +1,27 @@
+package com.rafaelpereiraramos.testeandroidv2.repo
+
+/**
+ * Created by Rafael P. Ramos on 18/11/2018.
+ */
+data class ResourceWrapper<out T>(val status: Status, val data: T?, val message: String?) {
+
+    enum class Status {
+        SUCCESS,
+        ERROR,
+        LOADING
+    }
+
+    companion object {
+        fun <T> success(data: T?): ResourceWrapper<T> {
+            return ResourceWrapper(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): ResourceWrapper<T> {
+            return ResourceWrapper(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): ResourceWrapper<T> {
+            return ResourceWrapper(Status.LOADING, data, null)
+        }
+    }
+}
