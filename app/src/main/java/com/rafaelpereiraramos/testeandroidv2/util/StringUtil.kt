19377d801b705ec1af4309d53bf43e86357810d8
Rafael R <raha148@hotmail.com>
2018-11-26 22:21:38 -0200

    Detalhes na tela de Statements

---
+++ app/src/main/java/com/rafaelpereiraramos/testeandroidv2/util/StringUtil.kt
@@ -0,0 +1,36 @@
+package com.rafaelpereiraramos.testeandroidv2.util
+
+import java.text.DecimalFormat
+import java.util.*
+
+/**
+ * Created by Rafael P. Ramos on 26/11/2018.
+ */
+class StringUtil {
+    companion object {
+
+        fun applyAgencyMask(agency: String?): String {
+            if (agency.isNullOrEmpty()) return  ""
+
+            return buildString {
+                append(agency)
+                insert(2, ".")
+                val lastCharPos = length
+                insert(lastCharPos - 1, "-")
+            }
+        }
+
+        fun applyMoneyMask(value: Float) = applyMoneyMask(value.toDouble())
+
+        fun applyMoneyMask(value: Double): String {
+            val formatter = DecimalFormat.getInstance() as DecimalFormat
+            formatter.maximumFractionDigits = 2
+            formatter.minimumFractionDigits = 2
+
+            val strValue = formatter.format(value)
+            val currencySymbol = Currency.getInstance(Locale.getDefault()).symbol
+
+            return "$currencySymbol $strValue"
+        }
+    }
+}
--- app/src/main/java/com/rafaelpereiraramos/testeandroidv2/view/statement/StatementsActivity.kt
+++ app/src/main/java/com/rafaelpereiraramos/testeandroidv2/view/statement/StatementsActivity.kt
@@ -3,12 +3,26 @@ package com.rafaelpereiraramos.testeandroidv2.view.statement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rafaelpereiraramos.testeandroidv2.R
+import com.rafaelpereiraramos.testeandroidv2.db.model.UserTO
+import com.rafaelpereiraramos.testeandroidv2.util.StringUtil.Companion.applyAgencyMask
+import com.rafaelpereiraramos.testeandroidv2.util.StringUtil.Companion.applyMoneyMask
+import kotlinx.android.synthetic.main.activity_statements.*
 
 class StatementsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statements)
+
+        val user: UserTO = intent.getParcelableExtra(USER_KEY)
+
+        setView(user)
+    }
+
+    private fun setView(userTO: UserTO) {
+        prompt_name.text = userTO.name
+        prompt_bankAccount.text = getString(R.string.prompt_user_bank_account, userTO.bankAccount, applyAgencyMask(userTO.agency))
+        prompt_balance.text = applyMoneyMask(userTO.balance)
     }
 
     companion object {
