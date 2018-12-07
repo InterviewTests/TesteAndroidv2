b292394b780f9da8f3c1088ea6fede84ad94993e
DIR\r.de.melo.silva <r.de.melo.silva@accenture.com>
2018-12-06 23:54:54 -0200

    Creates clickListener at login button

--- BankApp/app/src/main/java/br/com/rphmelo/bankapp/LoginActivity.kt
+++ BankApp/app/src/main/java/br/com/rphmelo/bankapp/LoginActivity.kt
@@ -1,12 +1,23 @@
 package br.com.rphmelo.bankapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginIntoApp()
+    }
+
+    private fun loginIntoApp() {
+        btnLogin.setOnClickListener {
+            val currencyActivity = Intent(this, CurrencyActivity::class.java)
+            startActivity(currencyActivity)
+        }
     }
 }
