b2d369f7e5956d92b16bd9b420063ca5ccc9e7c1
DIR\r.de.melo.silva <r.de.melo.silva@accenture.com>
2018-12-11 18:28:52 -0200

    Add GsonHelper for converting api response

---
+++ BankApp/app/src/main/java/br/com/rphmelo/bankapp/common/utils/GsonHelper.kt
@@ -0,0 +1,15 @@
+package br.com.rphmelo.bankapp.common.utils
+
+import br.com.rphmelo.bankapp.currency.domain.models.StatementModel
+import br.com.rphmelo.bankapp.login.domain.models.LoginRequest
+import br.com.rphmelo.bankapp.login.domain.models.LoginResponse
+import com.google.gson.Gson
+
+class GsonHelper {
+
+    fun toJson(value: Any): String? = Gson().toJson(value)
+
+    fun fromJsonLoginRequest(value: String): LoginRequest = Gson().fromJson(value, LoginRequest::class.java)
+    fun fromJsonLoginResponse(value: String): LoginResponse = Gson().fromJson(value, LoginResponse::class.java)
+
+}
