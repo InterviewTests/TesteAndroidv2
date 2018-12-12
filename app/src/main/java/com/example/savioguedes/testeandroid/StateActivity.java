6dd498f373f4efd3213010617f00e4ed1e2c851e
savio.guedes <savio.guedes@accenture.com>
2018-12-12 16:58:34 -0200

    lista de cards

--- app/src/main/java/com/example/savioguedes/testeandroid/StateActivity.java
+++ app/src/main/java/com/example/savioguedes/testeandroid/StateActivity.java
@@ -2,12 +2,45 @@ package com.example.savioguedes.testeandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.savioguedes.testeandroid.adapter.StateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StateActivity extends AppCompatActivity {
+
+    private RecyclerView stateRecyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager LayoutManager;
+
+
+    private List<String> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
+
+        stateRecyclerView = findViewById(R.id.state_recyclerview);
+        listItems = new ArrayList<>();
+
+        //config recyclerview
+        stateRecyclerView.setHasFixedSize(true);
+        LayoutManager = new LinearLayoutManager(this);
+        stateRecyclerView.setLayoutManager(LayoutManager);
+
+        listItems.add("");
+        listItems.add("");
+        listItems.add("");
+        listItems.add("");
+        listItems.add("");
+        listItems.add("");
+
+        //set adapter
+        adapter = new StateAdapter(listItems, this);
+        stateRecyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/savioguedes/testeandroid/adapter/StateAdapter.java
@@ -0,0 +1,60 @@
+package com.example.savioguedes.testeandroid.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.savioguedes.testeandroid.R;
+
+import java.util.List;
+
+public class StateAdapter extends RecyclerView.Adapter<StateAdapter.ViewLayout>{
+
+    private List<String> items;
+    private Context context;
+
+    public StateAdapter(List<String> items, Context context){
+
+        this.items = items;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewLayout onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.states_card_layout, parent, false);
+
+        ViewLayout view = new ViewLayout(v);
+
+        return view;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewLayout viewLayout, int i) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ViewLayout extends RecyclerView.ViewHolder{
+
+        TextView payTitle, payValue;
+
+        ViewLayout(@NonNull View itemView) {
+            super(itemView);
+
+            payTitle = itemView.findViewById(R.id.state_card_pay);
+            payValue = itemView.findViewById(R.id.state_card_value);
+        }
+    }
+}
