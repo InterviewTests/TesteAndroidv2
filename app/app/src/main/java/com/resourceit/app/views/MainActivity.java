cd9face25c7fd3bd3d31ebc9503133c62d202eb6
khalid <kaitbellahs@gmail.com>
2019-01-12 18:19:00 -0200

    View da página Login
    Lista:
    1. Adicionar permissão da internet
    2. adicionar logo do banco para assets
    3. Criando fragment do login
    4. Criando a logica para mudar entre fragments
    5. Criando Loading de espera

--- app/app/src/main/java/com/resourceit/app/views/MainActivity.java
+++ app/app/src/main/java/com/resourceit/app/views/MainActivity.java
@@ -1,16 +1,44 @@
 package com.resourceit.app.views;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.resourceit.app.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    @BindView(R.id.loading)
+    ConstraintLayout loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Fragment fragment = new LoginFragment();
+        updateFragment(fragment);
+        updateFragment(fragment);
+    }
+
+    public void updateFragment(Fragment destFragment) {
+        fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragments, destFragment);
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
+    }
+
+    public void Loading(Boolean visibility){
+        loading.setVisibility(visibility?View.VISIBLE:View.GONE);
     }
 }
