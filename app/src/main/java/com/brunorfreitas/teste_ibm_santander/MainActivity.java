45bab18b039948b77bdea407e3480b3ffcf78855
LegionDroid <brunolegion@hotmail.com>
2019-02-22 20:17:36 -0300

    Commit Inicial - Criacao do Projeto

---
+++ app/src/main/java/com/brunorfreitas/teste_ibm_santander/MainActivity.java
@@ -0,0 +1,13 @@
+package com.brunorfreitas.teste_ibm_santander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
