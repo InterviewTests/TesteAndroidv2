dff180b586aaef39bcbe28ee85812c33b21f3003
Kako Botasso <kakodev@gmail.com>
2019-03-06 16:16:20 -0300

    Setup login activity layout

--- app/src/main/java/br/com/kakobotasso/testeandroidv2/LoginActivity.java
+++ app/src/main/java/br/com/kakobotasso/testeandroidv2/LoginActivity.java
@@ -2,12 +2,15 @@ package br.com.kakobotasso.testeandroidv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
