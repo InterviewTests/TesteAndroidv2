e75d7e73858ab232176010b0ec627588706517a6
lazylinepainter <wingapede@hotmail.com>
2019-04-11 17:35:23 -0300

    *Adição de novas colors *Adição de novas strings *Criação do XML da tela Conta *Criação do item_atividades para RecyclerView *implementação de bibliotecas

--- app/src/main/java/com/example/santander/LoginActivity.java
+++ app/src/main/java/com/example/santander/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.santander;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initVar();
+    }
+
+    public void initVar() {
+        btLogin = findViewById(R.id.bt_login_enter);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ContaActivity.class));
+            }
+        });
     }
 }
