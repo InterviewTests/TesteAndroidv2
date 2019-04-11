9b32a4ce46311057715dc0be07fcf6f32b489a21
lazylinepainter <wingapede@hotmail.com>
2019-04-11 15:04:10 -0300

    *Adição de novas colors *Adição de novas strings *Criação do XML da tela Login *Remoção de warnings *Adição de novos drawables

--- app/src/main/java/com/example/santander/LoginActivity.java
+++ app/src/main/java/com/example/santander/LoginActivity.java
@@ -3,11 +3,11 @@ package com.example.santander;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
