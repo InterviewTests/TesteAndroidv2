d746c6199ca62201ff0c68fb9147f2002c39bf89
vitoria moraes <vicolmoraes@gmail.com>
2019-04-11 15:07:05 -0300

    criação de todos xml

--- app/src/main/java/com/santander/vicolmoraes/santander/View/LocationActivity.java
+++ app/src/main/java/com/santander/vicolmoraes/santander/View/LocationActivity.java
@@ -1,4 +1,15 @@
 package com.santander.vicolmoraes.santander.View;
 
-public class LocationActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.santander.vicolmoraes.santander.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
 }
