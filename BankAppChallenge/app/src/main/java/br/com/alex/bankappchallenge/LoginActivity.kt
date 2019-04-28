689850f4c2e8da78687d9cd61958450e2b6b5856
AlexSoaresDeSiqueira <alexsoaresdesiqueira@gmail.com>
2019-04-28 00:28:35 -0300

    change activity main to login

--- BankAppChallenge/app/src/main/java/br/com/alex/bankappchallenge/LoginActivity.kt
+++ BankAppChallenge/app/src/main/java/br/com/alex/bankappchallenge/LoginActivity.kt
@@ -3,10 +3,10 @@ package br.com.alex.bankappchallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
