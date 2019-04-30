9b8f5e02aaf007fffe9461fd3093d87292114a26
AlexSoaresDeSiqueira <alexsoaresdesiqueira@gmail.com>
2019-04-29 22:25:11 -0300

    statement instrumented local test

--- BankAppChallenge/app/src/androidTest/java/br/com/alex/bankappchallenge/feature/statement/StatementActivityTest.kt
+++ BankAppChallenge/app/src/androidTest/java/br/com/alex/bankappchallenge/feature/statement/StatementActivityTest.kt
@@ -1,4 +1,40 @@
 package br.com.alex.bankappchallenge.feature.statement
 
+import br.com.alex.bankappchallenge.rules.InstrumentedTestRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
 class StatementActivityTest {
+
+    @get:Rule
+    val instrumentedTestRule = InstrumentedTestRule(
+        StatementActivity::class.java,
+        beforeStatement = {
+            StatementRobot().mockUserAccount()
+        }
+    )
+
+    @Before
+    fun setup() {
+        statementActivityTest {
+            mockStatementList(instrumentedTestRule.serverRule)
+        }
+    }
+
+    @Test
+    fun shouldShowUserAccountData_whenOpenStatementActivity() {
+        statementActivityTest {
+            checkClientName("Jose da Silva Teste")
+            checkBankAgency("2050 / 01.231456-4")
+            checkBalance("R$ 3,34")
+        }
+    }
+
+    @Test
+    fun shouldShowStatementList_whenOpenStatementActivity() {
+        statementActivityTest {
+            checkStatementListIsDisplayed()
+        }
+    }
 }
--- BankAppChallenge/app/src/androidTest/java/br/com/alex/bankappchallenge/feature/statement/StatementRobot.kt
+++ BankAppChallenge/app/src/androidTest/java/br/com/alex/bankappchallenge/feature/statement/StatementRobot.kt
@@ -1,4 +1,64 @@
 package br.com.alex.bankappchallenge.feature.statement
 
-class StatementRobot {
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import br.com.alex.bankappchallenge.R
+import br.com.alex.bankappchallenge.network.model.UserAccount
+import br.com.alex.bankappchallenge.repository.LoginRepositoryContract
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.koin.test.KoinTest
+import org.koin.test.mock.declareMock
+import org.mockito.BDDMockito.given
+
+fun StatementActivityTest.statementActivityTest(
+    robot: StatementRobot.() -> Unit
+) {
+    StatementRobot().apply(robot)
+}
+
+class StatementRobot: KoinTest {
+
+    private fun textViewClientName() = R.id.textViewClientName
+    private fun textViewBankAgencyAccount() = R.id.textViewBankAccount
+    private fun textViewBalance() = R.id.textViewBalance
+    private fun recyclerViewStatement() = R.id.recyclerViewStatements
+
+    fun checkClientName(name: String) {
+        onView(withId(textViewClientName())).check(matches(withText(name)))
+    }
+
+    fun checkBankAgency(bankAgency: String) {
+        onView(withId(textViewBankAgencyAccount())).check(matches(withText(bankAgency)))
+    }
+
+    fun checkBalance(balance: String) {
+        onView(withId(textViewBalance())).check(matches(withText(balance)))
+    }
+
+    fun checkStatementListIsDisplayed() {
+        onView(withId(recyclerViewStatement())).check(matches(isDisplayed()))
+    }
+
+    fun mockUserAccount() {
+        declareMock<LoginRepositoryContract> {
+            given(this.getUserLogin())
+                .willReturn("user@gmail.com")
+
+            given(this.getUserAccount())
+                .willReturn(
+                    UserAccount(
+                        1,
+                        "Jose da Silva Teste",
+                        "2050",
+                        "012314564",
+                        3.3445)
+                )
+        }
+    }
+
+    fun mockStatementList(serverRule: MockWebServer) {
+        serverRule.enqueue(MockResponse().setBody(STATEMENT_SUCCESS))
+    }
 }
