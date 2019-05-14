f00936a991ba61fdb7a79783640ac0ed0ebd46a2
Gabriel Freire <42545425+gfreiresantos@users.noreply.github.com>
2019-05-14 14:17:46 -0300

    Change Main to Login, create login layout and colors added

--- app/src/main/java/com/example/testeandroidv2/LoginActivity.kt
+++ app/src/main/java/com/example/testeandroidv2/LoginActivity.kt
@@ -3,10 +3,10 @@ package com.example.testeandroidv2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
