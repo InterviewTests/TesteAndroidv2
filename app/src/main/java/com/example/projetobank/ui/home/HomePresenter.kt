72ae76a84d9d1bfe8ec3afb004284edbfb16e8a8
francelizemello <francelizemontteiro@gmail.com>
2019-05-14 23:17:16 -0300

    filtrar lista

--- app/src/main/java/com/example/projetobank/ui/home/HomePresenter.kt
+++ app/src/main/java/com/example/projetobank/ui/home/HomePresenter.kt
@@ -1,13 +1,16 @@
 package com.example.projetobank.ui.home
 
+import com.example.projetobank.data.model.UsuarioResposta
+import com.example.projetobank.data.source.CallbackResponse
 import com.example.projetobank.data.source.DadosBancarioDataSource
 import com.example.projetobank.data.source.UsuarioDataSource
 
 class HomePresenter
-
-    (val repositorio: DadosBancarioDataSource,
-     val repositorioUsuario: UsuarioDataSource,
-     val fragment: HomeContrato.View)
+    (
+    val repositorio: DadosBancarioDataSource,
+    val repositorioUsuario: UsuarioDataSource,
+    val fragment: HomeContrato.View
+)
 
     : HomeContrato.Presenter {
 
@@ -19,7 +22,16 @@ class HomePresenter
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-   private  fun listarDadosBancarioDoUsuario () {
-       
-   }
+    private fun listarDadosBancarioDoUsuario() {
+        repositorioUsuario.pegaUsuario(null, object : CallbackResponse<UsuarioResposta> {
+            override fun sucesso(response: UsuarioResposta) {
+                val usuario = response.usuario[0]
+
+            }
+
+            override fun erro() {
+
+            }
+        })
+    }
 }
