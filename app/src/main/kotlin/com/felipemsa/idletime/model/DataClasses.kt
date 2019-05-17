9d213826b87fa65e7ba23abda37d6cc6298c7499
felipemsa <felipe.fulgur@gmail.com>
2019-05-16 23:15:35 -0300

    data classes

---
+++ app/src/main/kotlin/com/felipemsa/idletime/model/DataClasses.kt
@@ -0,0 +1,11 @@
+package com.felipemsa.idletime.model
+
+data class LoginResponse(var userAccount: UserAccount, var error: Error)
+
+data class UserAccount(var userId: Int, var name: String, var bankAccount: String, var agency: String, var balance: Double)
+
+data class StatementsResponse(var statementList: List<Statement>)
+
+data class Statement(var title: String, var desc: String, var date: String, var value: Double)
+
+data class Error(var code: Int, var message: String)
--- app/src/main/kotlin/com/felipemsa/idletime/ui/MainActivity.kt
+++ app/src/main/kotlin/com/felipemsa/idletime/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.felipemsa.idletime
+package com.felipemsa.idletime.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.felipemsa.idletime.R
 
 class MainActivity : AppCompatActivity() {
 
