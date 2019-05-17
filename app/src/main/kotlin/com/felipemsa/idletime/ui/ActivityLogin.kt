1837e110de07b367b6e3674082b16639ed98d338
felipemsa <felipe.fulgur@gmail.com>
2019-05-17 11:43:22 -0300

    transaction screen layout file

--- app/src/main/kotlin/com/felipemsa/idletime/ui/ActivityLogin.kt
+++ app/src/main/kotlin/com/felipemsa/idletime/ui/ActivityLogin.kt
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.felipemsa.idletime.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityLogin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+
 
     }
 }
