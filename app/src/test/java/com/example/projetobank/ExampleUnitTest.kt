c223fdd1e78cf4b16513d3929bda011b95aa58af
Fran <francelize.monteiro@prontodigital.com.br>
2019-05-31 13:38:28 -0300

    adição do teste unitário

--- app/src/test/java/com/example/projetobank/ExampleUnitTest.kt
+++ app/src/test/java/com/example/projetobank/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.example.projetobank
 
+import com.example.projetobank.data.model.Usuario
+import com.example.projetobank.util.ehValido
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,43 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    @Test fun validarSenhaLetraMaiuscula() {
+        val usuario = Usuario("frann", "testeA")
+        assertTrue(usuario.ehValido {  })
     }
+
+    @Test fun validarSenhCaracterEspecial() {
+        val usuario = Usuario("frann", "teste@")
+        assertTrue(usuario.ehValido {  })
+    }
+    @Test fun validarSenhaAlphanumerica() {
+        val usuario = Usuario("frann", "teste3")
+        assertTrue(usuario.ehValido {  })
+    }
+
+    @Test fun validarSenhaAlphanumericaComLetraMaiscula() {
+        val usuario = Usuario("frann", "tes3Te")
+        assertTrue(usuario.ehValido {  })
+    }
+
+    @Test fun validarSenhaAlphanumericaComCaracterEspecial() {
+        val usuario = Usuario("fran", "3@")
+        assertTrue(usuario.ehValido {  })
+    }
+
+    @Test fun validarSenhaMaisculaComCaracterEspecial() {
+        val usuario = Usuario("frann@gmail.com", "M@")
+        assertTrue(usuario.ehValido {  })
+    }
+    @Test fun validarSenhaMaisculaComCaracterEspecialEAlphanumerica() {
+        val usuario = Usuario("frannzita", "M@3")
+        assertTrue(usuario.ehValido {  })
+    }
+
+    @Test fun validarUsuarioCampoVazio() {
+        val usuario = Usuario("", "M@3")
+        assertTrue(usuario.ehValido {  })
+    }
+
 }
