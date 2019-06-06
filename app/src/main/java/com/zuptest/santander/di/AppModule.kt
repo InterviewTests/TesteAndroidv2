661abf5e89478759ee98b353f3b5775cf7a0ef8a
Gustavo Lima <gustavosnl@gmail.com>
2019-06-06 01:08:23 -0300

    setup da camada de integração com API

--- app/src/main/java/com/zuptest/santander/di/AppModule.kt
+++ app/src/main/java/com/zuptest/santander/di/AppModule.kt
@@ -1,10 +1,40 @@
 package com.zuptest.santander.di
 
+import com.zuptest.data.api.Api
+import com.zuptest.santander.BuildConfig
+import com.zuptest.santander.BuildConfig.DEBUG
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 object AppModule {
 
     val module = module {
 
+        single<Retrofit> {
+            Retrofit.Builder()
+                .baseUrl(BuildConfig.SERVER_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(get())
+                .build()
+        }
+
+        single<Api> {
+            (get() as Retrofit).create(Api::class.java)
+        }
+
+        single<OkHttpClient> {
+            val interceptor = HttpLoggingInterceptor()
+            if (DEBUG) {
+                interceptor.level = HttpLoggingInterceptor.Level.BODY
+            } else {
+                interceptor.level = HttpLoggingInterceptor.Level.NONE
+            }
+            OkHttpClient.Builder().addInterceptor(interceptor).build()
+        }
     }
 }
