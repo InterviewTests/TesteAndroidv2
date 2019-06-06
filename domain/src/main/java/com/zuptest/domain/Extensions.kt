6c934b6b9002cdb0047b62718e33ef132fb0a780
Gustavo Lima <gustavosnl@gmail.com>
2019-06-06 03:48:59 -0300

    extensão para Date

--- domain/src/main/java/com/zuptest/domain/Extensions.kt
+++ domain/src/main/java/com/zuptest/domain/Extensions.kt
@@ -1,2 +1,9 @@
 package com.zuptest.domain
 
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Date.formattedPrint(): String {
+    val fullDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
+    return fullDateFormat.format(this)
+}
