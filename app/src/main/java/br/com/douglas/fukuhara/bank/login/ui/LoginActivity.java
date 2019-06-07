3b11ca817ee125a93b956b84632719ff43b422ce
Douglas Fukuhara <dofukuhara@gmail.com>
2019-06-07 09:13:27 -0300

    [Bank] Initial Commit
     - Removed non-used files
     - Included gitignore file
     - Included initial Android Studio project

---
+++ app/src/main/java/br/com/douglas/fukuhara/bank/login/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.douglas.fukuhara.bank.login.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.douglas.fukuhara.bank.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
