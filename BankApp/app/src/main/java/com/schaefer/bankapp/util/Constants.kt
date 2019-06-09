6088e56c29c4b1f3d78c7e6eb372cf3126dff47c
Artur Schaefer <artur.schaefer@hotmail.com>
2019-06-08 23:10:59 -0300

    refactor: add constants to a file

---
+++ BankApp/app/src/main/java/com/schaefer/bankapp/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.schaefer.bankapp.util
+
+class Constants {
+    companion object {
+        val DATE_SERVER = "yyyy-MM-dd"
+        val DATE_FORMATED = "dd/MM/yyyy"
+
+        val SHARED_LOGIN = "login"
+        val SHARED_HAS_LOGIN = "hasLogin"
+        val SHARED_USERNAME = "user"
+        val SHARED_PASSWORD = "password"
+        val SHARED_USER_ID = "user_id"
+
+        val EXTRA_USER = "user"
+    }
+}
