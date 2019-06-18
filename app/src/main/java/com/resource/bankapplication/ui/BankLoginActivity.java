fa15ba8ae5c2f4734987af9717c83d7251e402d6
Gerson <gersonjunior1771@gmail.com>
2019-06-18 19:35:43 -0300

    Primeiro commit do projeto
    Foi criado o projeto e iniciado a criação do layout da tela de login.

---
+++ app/src/main/java/com/resource/bankapplication/ui/BankLoginActivity.java
@@ -0,0 +1,16 @@
+package com.resource.bankapplication.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.resource.bankapplication.R;
+
+public class BankLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_login);
+    }
+}
