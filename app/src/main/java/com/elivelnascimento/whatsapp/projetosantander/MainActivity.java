98c13aae0e4d2986d863bae20024d43765435256
Elivel <elivelsantos81@gmail.com>
2019-06-19 11:47:43 -0300

    Criação projeto
    Foi criado o projeto para o Santander.

---
+++ app/src/main/java/com/elivelnascimento/whatsapp/projetosantander/MainActivity.java
@@ -0,0 +1,13 @@
+package com.elivelnascimento.whatsapp.projetosantander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
