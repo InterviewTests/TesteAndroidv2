7066a23e959aab4b0a5f3831835cb229826f6608
kayque <kayque_rodrigues7@hotmail.com>
2019-06-20 02:09:30 -0300

    alter value entity

--- SantanderApp/app/src/main/java/com/accenture/santander/entity/Statement.kt
+++ SantanderApp/app/src/main/java/com/accenture/santander/entity/Statement.kt
@@ -2,7 +2,7 @@ package com.accenture.santander.entity
 
 data class Statement(
     var title: String = "",
-    var desc: String = "Joao Alfredo",
-    var date: String = "2018-07-25",
-    var value: Double = 745.03
+    var desc: String = "",
+    var date: String = "",
+    var value: Double = 0.0
 )
