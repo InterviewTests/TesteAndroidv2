6ff6655da3667f2db5ddf473e375906ddbc55a04
Guilherme <gmattos505@gmail.com>
2019-06-24 09:00:33 -0300

    Layout do Login e Colors
    O layout da tela de login foi executado com os componentes de acordo com os arboards disponibilizados. Foi adicionado as cores do projeto dentro do Values (colors.xml).

--- app/src/main/java/com/example/santanderapplication/BankLoginActivity.java
+++ app/src/main/java/com/example/santanderapplication/BankLoginActivity.java
@@ -1,13 +1,20 @@
 package com.example.santanderapplication;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class BankLoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        setContentView( R.layout.activity_bank_login );
+
+
+
     }
 }
