b545556781ba9bae6987e191b11ed5fed5d8f4b7
Guilherme Sartori <guilhermesassarico@gmail.com>
2019-06-25 13:02:52 -0300

    Ajustes layout, intents e mostrar recycler
    defini intents e recycler na mão para aparecer na tela

--- app/src/main/java/com/example/santandertestebank/ui/BankPaymentsActivity.java
+++ app/src/main/java/com/example/santandertestebank/ui/BankPaymentsActivity.java
@@ -1,11 +1,15 @@
-package com.example.santandertestebank;
+package com.example.santandertestebank.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.santandertestebank.R;
 import com.example.santandertestebank.adapter.AdapterBankPayments;
 import com.example.santandertestebank.model.Payments;
 
@@ -27,29 +31,45 @@ public class BankPaymentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_bank_payments);
+        getSupportActionBar ().hide ();
 
         loadUI ();
 
-        this.createPaymet ();
+        this.createPayment ();
 
         AdapterBankPayments adapterBankPayments = new AdapterBankPayments (paymentsList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager
+                (getApplicationContext ());
 
+        recyclerViewPayments.setLayoutManager (layoutManager);
         recyclerViewPayments.setHasFixedSize (true);
         recyclerViewPayments.setAdapter (adapterBankPayments);
 
+        imageViewLogout.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                logoutApp ();
+            }
+        });
+
     }
 
-    public void createPaymet(){
+    private void logoutApp() {
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity (intent);
+        finish ();
+    }
+
+    public void createPayment() {
 
         Payments payment = new Payments ("Pagamento", "Conta de Luz", "12/12/18", 1000.00);
         paymentsList.add (payment);
 
-        Payments payment2 = new Payments ("Pagamento", "Conta de Luz", "12/12/18", 1000.00);
-        paymentsList.add (payment);
+        Payments payment2 = new Payments ("Pagamento", "Conta de Luz", "12/12/18", 2000.00);
+        paymentsList.add (payment2);
 
-        Payments payment3 = new Payments ("Pagamento", "Conta de Luz", "12/12/18", 1000.00);
-        paymentsList.add (payment);
-
+        Payments payment3 = new Payments ("Pagamento", "Conta de Luz", "12/12/18", 3000.00);
+        paymentsList.add (payment3);
     }
 
     private void loadUI() {
@@ -57,6 +77,6 @@ public class BankPaymentsActivity extends AppCompatActivity {
         textViewBankAccount = findViewById (R.id.text_view_bank_account);
         textViewAccountBalance = findViewById (R.id.text_view_account_balance);
         imageViewLogout = findViewById (R.id.image_view_logout);
-        recyclerViewPayments = findViewById (R.id.recyclerViewPayments);
+        recyclerViewPayments = findViewById (R.id.recycler_view_payments);
     }
 }
--- app/src/main/java/com/example/santandertestebank/ui/LoginActivity.java
+++ app/src/main/java/com/example/santandertestebank/ui/LoginActivity.java
@@ -1,9 +1,13 @@
-package com.example.santandertestebank;
+package com.example.santandertestebank.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.santandertestebank.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,8 +19,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_login);
+        getSupportActionBar ().hide ();
 
         loadUI ();
+
+        buttonLogin.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                openPaymentList ();
+            }
+        });
+    }
+
+    private void openPaymentList() {
+        Intent intent = new Intent (this, BankPaymentsActivity.class);
+        startActivity (intent);
     }
 
     private void loadUI() {
