0a4759dae88256d5aaf2d0fde1daceabe1bcc19e
Lucas Silva <lucass.silva@me.com>
2019-07-05 01:35:42 -0300

    Design da tela de Login  * Criado tema para tela de login;  * Criado values/strings para texto da tela de login;  * Criado values/colors para cores da tela de login;  * Criado values/dimens para configuração e posicionamento dos elementos na tela de login;  * Implementado XML da tela de login;  * Adicionado imagem da logomarca do app;  * Criado xml de formato para botões e formulários;  * Alterado configurações referentes a LoginActivityno AndroidManifest.

--- app/src/main/java/ssilvalucas/testeandroidv2/screen/login/LoginActivity.java
+++ app/src/main/java/ssilvalucas/testeandroidv2/screen/login/LoginActivity.java
@@ -1,13 +1,15 @@
-package ssilvalucas.testeandroidv2;
+package ssilvalucas.testeandroidv2.screen.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ssilvalucas.testeandroidv2.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
