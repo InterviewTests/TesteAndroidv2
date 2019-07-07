3618d068b62e1d94a78754551664d9ce108135ee
dayloncosta <dayloncosta@hotmail.com>
2019-07-06 21:37:08 -0300

    Testes para login

---
+++ app/src/test/java/com/projeto/testedevandroidsantander/ui/loginScreen/LoginRouterUnitTest.java
@@ -0,0 +1,22 @@
+package com.projeto.testedevandroidsantander.ui.loginScreen;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+
+@RunWith(RobolectricTestRunner.class)
+public class LoginRouterUnitTest {
+    public static String TAG = LoginRouterUnitTest.class.getSimpleName();
+
+    @Before
+    public void setUp(){
+
+    }
+    @After
+    public void tearDown(){
+
+    }
+}
