775dab5bfcef5fb642b4b0787df6630678b273e3
Almeida <matheus.h.almeida@accenture.com>
2019-07-31 16:53:15 -0300

    Renomeando classe de testes para ControleLogin

--- App/app/src/test/java/com/example/testesantander/ControleLoginTest.java
+++ App/app/src/test/java/com/example/testesantander/ControleLoginTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ControleLoginTest {
     private Usuario user;
     private ControleLogin controlador;
 
