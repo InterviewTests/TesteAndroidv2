ed36a8bfc797bac8fec781b3d4412536eeda9edf
MatheusHenAlmeida <matheushdealmeida@gmail.com>
2019-08-08 20:54:02 -0300

    Implementado cabecalho da tela de detalhe

--- App/app/src/main/java/com/example/testesantander/DetailActivity.java
+++ App/app/src/main/java/com/example/testesantander/DetailActivity.java
@@ -6,21 +6,28 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import Models.UserAccount;
 
 public class DetailActivity extends AppCompatActivity {
-
+    private TextView tvSaldo;
+    private TextView tvConta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        getSupportActionBar().setElevation(0);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.azul));
         }
         Intent intent = getIntent();
         UserAccount userAccount = (UserAccount) intent.getSerializableExtra("userAccount");
         setTitle(userAccount.getName());
+        tvSaldo = findViewById(R.id.tvSaldo);
+        tvConta = findViewById(R.id.tvConta);
+        tvSaldo.setText(String.format("R$ %.2f", userAccount.getBalance()));
+        tvConta.setText(String.format("%s / %s", userAccount.getAgency(), userAccount.getBankAccount()));
     }
 }
