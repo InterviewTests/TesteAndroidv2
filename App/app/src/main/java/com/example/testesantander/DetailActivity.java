ac1747cc1aa15c6277a243c2c3f63a033d33705a
MatheusHenAlmeida <matheushdealmeida@gmail.com>
2019-08-09 13:00:33 -0300

    Criado botao de logoff

--- App/app/src/main/java/com/example/testesantander/DetailActivity.java
+++ App/app/src/main/java/com/example/testesantander/DetailActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,5 +31,20 @@ public class DetailActivity extends AppCompatActivity {
         tvConta = findViewById(R.id.tvConta);
         tvSaldo.setText(String.format("R$ %.2f", userAccount.getBalance()));
         tvConta.setText(String.format("%s / %s", userAccount.getAgency(), userAccount.getBankAccount()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.logoff_button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logoff) {
+            Intent it = new Intent(this, LoginActivity.class);
+            startActivity(it);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
