3bee4fd6c5d99687e0b0317d0ab6757850dcdcb0
Marcelo Mello <marcelo.mello18@gmail.com>
2020-01-22 23:57:50 -0300

    MSBM-01 - Add initial API structure for login

--- Bank/app/src/main/java/com/msbm/bank/loginScreen/LoginApi.java
+++ Bank/app/src/main/java/com/msbm/bank/loginScreen/LoginApi.java
@@ -1,7 +1,13 @@
 package com.msbm.bank.loginScreen;
 
-public class LoginApi {
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
-    public static String TAG = LoginApi.class.getSimpleName();
+public interface LoginApi {
 
+    @FormUrlEncoded
+    @POST("/login")
+    Call<LoginResponse> login(@Field("user") String user, @Field("password") String password);
 }
