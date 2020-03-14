9557851fe25e126d8d102212dd5d83877a21176e
Vitor Pacheco <vpacheco.costa@gmail.com>
2020-03-14 02:27:16 -0300

    Layout da tela de extrato

--- app/src/main/java/dev/vitorpacheco/solutis/bankapp/MainActivity.kt
+++ app/src/main/java/dev/vitorpacheco/solutis/bankapp/MainActivity.kt
@@ -1,12 +1,80 @@
 package dev.vitorpacheco.solutis.bankapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MovimentacoesAdapter(
+            arrayOf(
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado",
+                "teste",
+                "testando",
+                "testado"
+            )
+        )
+
+        recyclerView = lista_movimentacoes.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
+}
+
+class MovimentacoesAdapter(private val data: Array<String>) :
+    RecyclerView.Adapter<MovimentacoesAdapter.MovimentacoesViewHolder>() {
+
+    class MovimentacoesViewHolder(val cardView: MaterialCardView) :
+        RecyclerView.ViewHolder(cardView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovimentacoesViewHolder {
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_statement, parent, false) as MaterialCardView
+
+        return MovimentacoesViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: MovimentacoesViewHolder, position: Int) {
+//        holder.cardView.item_movimentacao_label.text = data[position]
+    }
+
+    override fun getItemCount() = data.size
+
 }
