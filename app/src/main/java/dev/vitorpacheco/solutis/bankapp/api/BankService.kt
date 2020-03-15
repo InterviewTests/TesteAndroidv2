9e45499a42c45d043904fbad98b5e7e219c55703
Vitor Pacheco <vpacheco.costa@gmail.com>
2020-03-14 23:31:31 -0300

    Configuração do interceptor de logging do retrofit

--- app/src/main/java/dev/vitorpacheco/solutis/bankapp/api/BankService.kt
+++ app/src/main/java/dev/vitorpacheco/solutis/bankapp/api/BankService.kt
@@ -1,15 +1,31 @@
 package dev.vitorpacheco.solutis.bankapp.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class BankService {
-    fun create(): BankApi {
-        val retrofit = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl("https://bank-app-test.herokuapp.com/api/")
-            .build()
 
-        return retrofit.create(BankApi::class.java)
+    companion object {
+        fun create(): BankApi {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            logging.redactHeader("Authorization")
+            logging.redactHeader("Cookie")
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://bank-app-test.herokuapp.com/api/")
+                .build()
+
+            return retrofit.create(BankApi::class.java)
+        }
     }
+
 }
