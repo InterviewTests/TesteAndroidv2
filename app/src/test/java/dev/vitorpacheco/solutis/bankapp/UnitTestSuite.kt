d954bdfa168c2ab8a1a3be8c4d5cfe2831e064f2
Vitor Pacheco <vpacheco.costa@gmail.com>
2020-03-15 21:10:36 -0300

    Atualização da suite dos testes unitários

--- app/src/test/java/dev/vitorpacheco/solutis/bankapp/UnitTestSuite.kt
+++ app/src/test/java/dev/vitorpacheco/solutis/bankapp/UnitTestSuite.kt
@@ -4,6 +4,7 @@ import dev.vitorpacheco.solutis.bankapp.extensions.BigDecimalExtensionKtTest
 import dev.vitorpacheco.solutis.bankapp.extensions.DateExtensionKtTest
 import dev.vitorpacheco.solutis.bankapp.loginScreen.LoginInteractorTest
 import dev.vitorpacheco.solutis.bankapp.loginScreen.LoginPresenterUnitTest
+import dev.vitorpacheco.solutis.bankapp.statementsScreen.StatementsInteractorTest
 import dev.vitorpacheco.solutis.bankapp.statementsScreen.StatementsPresenterUnitTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -14,6 +15,7 @@ import org.junit.runners.Suite
     LoginInteractorTest::class,
 
     StatementsPresenterUnitTest::class,
+    StatementsInteractorTest::class,
 
     BigDecimalExtensionKtTest::class,
     DateExtensionKtTest::class
