3031f677413d5c0d143fade7313b2be4d5bb74aa
leonardo <leonardo.ornelas@gmail.com>
2020-03-19 00:14:17 -0300

    Mapper do UI converte o model de dominio para model de view

--- bankapp/app/src/main/java/dev/ornelas/bankapp/ui/login/Mapper.kt
+++ bankapp/app/src/main/java/dev/ornelas/bankapp/ui/login/Mapper.kt
@@ -2,8 +2,6 @@ package dev.ornelas.bankapp.ui.login
 
 import dev.ornelas.banckapp.domain.model.Mapper
 import dev.ornelas.banckapp.domain.model.User
-import dev.ornelas.bankapp.data.datasource.api.retrofit.model.LoginResponseApi
-import dev.ornelas.bankapp.data.repository.ApiToUserModel
 
 object UserToUIMapper : Mapper<User, LoggedInUserView> {
     override fun map(type: User?): LoggedInUserView {
