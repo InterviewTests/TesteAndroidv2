53a4dc559542caf2cee698b815c0358b6afa78cb
Paulo Felipe <p.felipe@msn.com>
2020-03-21 16:16:53 -0300

    Created project

---
+++ app/src/main/java/com/example/ibm_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ibm_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
