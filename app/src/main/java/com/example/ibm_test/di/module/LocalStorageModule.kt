aa235f3df170c6b6b76fcf358d88c922a6927b34
Paulo Felipe <p.felipe@msn.com>
2020-03-23 03:23:08 -0300

    Added crypton to shared preferences

--- app/src/main/java/com/example/ibm_test/di/module/LocalStorageModule.kt
+++ app/src/main/java/com/example/ibm_test/di/module/LocalStorageModule.kt
@@ -1,16 +1,28 @@
 package com.example.ibm_test.di.module
 
 import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.example.ibm_test.localstorage.UserStorage
 import com.example.ibm_test.localstorage.UserStorageImp
 import dagger.Module
 import dagger.Provides
 
+
 @Module(includes = [ApplicationModule::class])
 class LocalStorageModule {
     @Provides
     fun provideUserStorage(context: Context) : UserStorage{
-        val sharedPreferences= context.getSharedPreferences(UserStorageImp.USER_INFO, Context.MODE_PRIVATE)
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            UserStorageImp.USER_INFO,
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
         return UserStorageImp(sharedPreferences)
     }
 }
