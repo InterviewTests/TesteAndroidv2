cdd7e47fab0c9e56962be6ee5128fe2f63d84ba1
Walison Azeredo Silva <walison.azeredo.silva@everis.com>
2020-04-07 00:09:55 -0300

    Adicionando a ViewModel da tela de Login

---
+++ App/app/src/main/java/com/example/testeandroidv2/viewModel/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.testeandroidv2.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.testeandroidv2.data.ApiService
+import retrofit2.Call
+import retrofit2.Response
+
+class LoginViewModel : ViewModel() {
+}
