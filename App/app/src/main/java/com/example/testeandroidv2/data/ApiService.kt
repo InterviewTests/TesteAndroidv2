7dae8f37788e320e677ae8e611b078aa019ead73
Walison Azeredo Silva <walison.azeredo.silva@everis.com>
2020-04-06 23:49:38 -0300

    Adicionando a classe ApiService onde será inicializado o retrofit

---
+++ App/app/src/main/java/com/example/testeandroidv2/data/ApiService.kt
@@ -0,0 +1,18 @@
+package com.example.testeandroidv2.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class ApiService {
+
+    private fun initRetroFit(): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl("https://bank-app-test.herokuapp.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val service = initRetroFit().create(BankServices::class.java)
+
+}
