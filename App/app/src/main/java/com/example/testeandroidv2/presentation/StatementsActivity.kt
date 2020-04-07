167b63b0fcc77e872fe0eebae03aee9223878362
Walison Azeredo Silva <walison.azeredo.silva@everis.com>
2020-04-07 01:02:07 -0300

    Adicionando a activity de statements

---
+++ App/app/src/main/java/com/example/testeandroidv2/presentation/StatementsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testeandroidv2.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testeandroidv2.R
+
+class StatementsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statements)
+
+
+    }
+}
---
+++ App/app/src/main/java/com/example/testeandroidv2/viewModel/StatementsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.testeandroidv2.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class StatementsViewModel : ViewModel() {
+
+}
