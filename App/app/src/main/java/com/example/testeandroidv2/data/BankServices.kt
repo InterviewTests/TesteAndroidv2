87c24581455dc83426106525ddfcd1ced54f0f6e
Walison Azeredo Silva <walison.azeredo.silva@everis.com>
2020-04-07 12:33:48 -0300

    Adequação bankServices para passar o id do usuário por parametro

--- App/app/src/main/java/com/example/testeandroidv2/data/BankServices.kt
+++ App/app/src/main/java/com/example/testeandroidv2/data/BankServices.kt
@@ -1,18 +1,20 @@
 package com.example.testeandroidv2.data
 
+import com.example.testeandroidv2.model.LoginBodyResponse
 import com.example.testeandroidv2.model.StatementsBodyResponse
 import com.example.testeandroidv2.model.User
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface BankServices {
 
     @POST("login")
-    fun login(@Body user: User?): Call<User?>?
+    fun login(@Body user: User): Call<LoginBodyResponse>
 
-    @GET("statements/1")
-    fun getStatements(): Call<StatementsBodyResponse>
+    @GET("statements/{id}")
+    fun getStatements(@Path("id") id: Int): Call<StatementsBodyResponse>
 
 }
