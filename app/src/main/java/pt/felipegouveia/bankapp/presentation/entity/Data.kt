c123155960af349269b94de8d2db50cd346c6a9e
Felipe Gouveia <felipegouveia3@gmail.com>
2020-04-09 02:08:35 +0100

    [TDD] Added implementation of LoginViewModel

--- app/src/main/java/pt/felipegouveia/bankapp/presentation/entity/Data.kt
+++
@@ -1,8 +0,0 @@
-package pt.felipegouveia.bankapp.presentation.entity
-
-/**
- * A generic wrapper class around data request
- */
-data class Data<RequestData>(var responseType: Status, var data: RequestData? = null, var error: Error? = null)
-
-enum class Status { SUCCESSFUL, ERROR }
---
+++ app/src/main/java/pt/felipegouveia/bankapp/presentation/entity/Response.kt
@@ -0,0 +1,8 @@
+package pt.felipegouveia.bankapp.presentation.entity
+
+/**
+ * A generic wrapper class around API requests
+ */
+data class Response<RequestData>(var status: Status, var data: RequestData? = null, var error: Error? = null)
+
+enum class Status { SUCCESSFUL, ERROR }
