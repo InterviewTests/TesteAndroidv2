a9eb758b7d045ce33d4206f7ce999c7058577e82
Raphael Marques <raphael.marques@nuvem.net>
2020-04-26 14:23:06 -0400

    Navigation, implementando navegação

--- Everis/app/src/main/java/br/com/raphael/everis/MainActivity.kt
+++ Everis/app/src/main/java/br/com/raphael/everis/MainActivity.kt
@@ -2,11 +2,17 @@ package br.com.raphael.everis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.navHost)
+        setupActionBarWithNavController(navController)
+
     }
 }
