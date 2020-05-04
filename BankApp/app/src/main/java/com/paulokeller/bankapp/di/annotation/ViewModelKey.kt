da2c799e495ed5d7846d7ebe5af91a21194ff2fa
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-05-04 20:04:44 -0300

    remove trash

--- BankApp/app/src/main/java/com/paulokeller/bankapp/di/annotation/ViewModelKey.kt
+++
@@ -1,9 +0,0 @@
-package com.paulokeller.bankapp.di.annotation
-
-import androidx.lifecycle.ViewModel
-import dagger.MapKey
-import kotlin.reflect.KClass
-
-@MapKey
-@Target(AnnotationTarget.FUNCTION)
-annotation class ViewModelKey(val value : KClass<out ViewModel>)
