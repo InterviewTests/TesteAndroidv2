c1ddd5c1263cac339a7397ebc8e4e3d62768e7d8
caroltr <caroltr.srs@gmail.com>
2020-05-04 16:53:57 -0300

    Improvements on code and add tag on manifest to prevent webservice call when rotation changes

--- Bank/app/src/main/java/com/tata/bank/repository/Repository.kt
+++ Bank/app/src/main/java/com/tata/bank/repository/Repository.kt
@@ -6,15 +6,13 @@ import com.tata.bank.login.LoginCredentials
 import com.tata.bank.security.SecurityWorker
 import com.tata.bank.security.SecurityWorkerLegacy
 
-class Repository(private val context: Context) {
+class Repository(context: Context) {
 
-    private val preferences by lazy { Preferences(context) }
-    private val security by lazy {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            SecurityWorker(context)
-        } else {
-            SecurityWorkerLegacy(context)
-        }
+    private val preferences = Preferences(context)
+    private val security = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        SecurityWorker(context)
+    } else {
+        SecurityWorkerLegacy(context)
     }
 
     fun saveCredentials(loginCredentials: LoginCredentials) {
