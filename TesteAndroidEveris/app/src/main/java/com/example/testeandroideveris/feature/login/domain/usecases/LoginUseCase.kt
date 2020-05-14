c5e4d9fe0e0b1805fe9538f5e53fc5ee395de820
ruan.queiroz <ruanromulo20@gmail.com>
2020-05-14 17:43:55 -0300

    Adicionando validacao de cpf

--- TesteAndroidEveris/app/src/main/java/com/example/testeandroideveris/feature/login/domain/usecases/LoginUseCase.kt
+++ TesteAndroidEveris/app/src/main/java/com/example/testeandroideveris/feature/login/domain/usecases/LoginUseCase.kt
@@ -5,6 +5,8 @@ import com.example.testeandroideveris.feature.login.data.LoginRequestData
 import com.example.testeandroideveris.feature.login.data.LoginResponseData
 import com.example.testeandroideveris.feature.login.domain.repository.LoginRepository
 import com.example.testeandroideveris.feature.login.mappers.UserAccountMapper
+import com.example.testeandroideveris.feature.util.BankUtil.digitCalc
+import com.example.testeandroideveris.feature.util.BankUtil.weightCPF
 import kotlinx.coroutines.flow.map
 import java.util.regex.Pattern
 
@@ -17,7 +19,7 @@ class LoginUseCase(private val repository: LoginRepository){
 
     fun validate(loginRequest: LoginRequestData) = when {
         loginRequest.user.isBlank() -> LoginDataState.INVALID_USER
-        !android.util.Patterns.EMAIL_ADDRESS.matcher(loginRequest.user).matches() -> LoginDataState.INVALID_USER
+        validEmailOrDocument(loginRequest) -> LoginDataState.INVALID_USER
         loginRequest.password.isBlank() -> LoginDataState.INVALID_PASSWORD
         !isValidPassword(loginRequest.password) -> LoginDataState.INVALID_PASSWORD
         else -> LoginDataState.VALID_SUCCESS
@@ -28,5 +30,20 @@ class LoginUseCase(private val repository: LoginRepository){
             "^(?=.*[A-Z])(?=.*[@_.]).*\$"
         )
         return PASSWORD_PATTERN.matcher(password).matches()
+    }
+
+    private fun validEmailOrDocument(loginRequest: LoginRequestData): Boolean {
+        return if (loginRequest.user.contains("@", false)) {
+            !android.util.Patterns.EMAIL_ADDRESS.matcher(loginRequest.user).matches()
+        } else {
+            !isValidCPF(loginRequest.user)
+        }
+    }
+
+    private fun isValidCPF(document: String?): Boolean {
+        if (document == null || document.length != 11) return false
+        val digit1: Int = digitCalc(document.substring(0, 9), weightCPF)
+        val digit2: Int = digitCalc(document.substring(0, 9) + digit1, weightCPF)
+        return document == document.substring(0, 9) + digit1 + digit2
     }
 }
--- TesteAndroidEveris/app/src/main/java/com/example/testeandroideveris/feature/util/BankUtil.kt
+++ TesteAndroidEveris/app/src/main/java/com/example/testeandroideveris/feature/util/BankUtil.kt
@@ -5,6 +5,7 @@ import java.util.*
 
 object BankUtil {
     private var DATE_FORMAT = "dd/MM/yyyy"
+    val weightCPF = intArrayOf(11, 10, 9, 8, 7, 6, 5, 4, 3, 2)
 
     fun formatAccountNumber(agency: String, bankAccount: String) = String.format("%s / %s", agency, bankAccount)
 
@@ -25,6 +26,19 @@ object BankUtil {
 
     private fun getDefaultLocale(): Locale{
         return Locale("pt", "BR")
+    }
+
+    fun digitCalc(str: String, weight: IntArray): Int {
+        var sum = 0
+        var index = str.length - 1
+        var digit: Int
+        while (index >= 0) {
+            digit = str.substring(index, index + 1).toInt()
+            sum += digit * weight[weight.size - str.length + index]
+            index--
+        }
+        sum = 11 - sum % 11
+        return if (sum > 9) 0 else sum
     }
 }
 
