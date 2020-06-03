b875f33693835abc38ab4bc95295d5cfc2b1691b
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-06-03 19:14:29 -0300

    Escondendo o teclado ao clicar fora do campo de texto

--- app/src/main/java/com/gft/testegft/base/BaseActivity.java
+++ app/src/main/java/com/gft/testegft/base/BaseActivity.java
@@ -1,6 +1,12 @@
 package com.gft.testegft.base;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
@@ -28,4 +34,22 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected abstract void observe();
 
     protected abstract void attachViewModel();
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    assert imm != null;
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
 }
