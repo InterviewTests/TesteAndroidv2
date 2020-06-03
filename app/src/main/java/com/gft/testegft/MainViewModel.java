5d5f397af8f232ef7259e81175df05c9369abdde
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-06-03 19:14:11 -0300

    Layout da tela de login implementado

--- app/src/main/java/com/gft/testegft/MainViewModel.java
+++ app/src/main/java/com/gft/testegft/MainViewModel.java
@@ -6,13 +6,22 @@ import com.gft.testegft.base.BaseViewModel;
 
 public class MainViewModel extends BaseViewModel {
 
-    private MutableLiveData<String> nome = new MutableLiveData<>();
+    private MutableLiveData<String> user = new MutableLiveData<>();
+    private MutableLiveData<String> password = new MutableLiveData<>();
 
-    public MutableLiveData<String> getNome() {
-        return nome;
+    public MutableLiveData<String> getUser() {
+        return user;
     }
 
-    public void setNome(String novoNome) {
-        nome.setValue(novoNome);
+    public void setUser(String newUser) {
+        user.setValue(newUser);
+    }
+
+    public MutableLiveData<String> getPassword() {
+        return password;
+    }
+
+    public void setPassword(MutableLiveData<String> password) {
+        this.password = password;
     }
 }
