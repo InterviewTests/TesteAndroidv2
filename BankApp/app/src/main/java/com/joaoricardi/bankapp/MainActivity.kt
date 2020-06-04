91c7fc919c46714f9ab852e344a27316b75d7a82
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-04 12:40:21 -0300

    criando login fragment e iniciando com ele

--- BankApp/app/src/main/java/com/joaoricardi/bankapp/MainActivity.kt
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.joaoricardi.bankapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joaoricardi.bankapp.feature.LoginFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setUpFragment(){
+        supportFragmentManager.beginTransaction()
+                .replace(
+                        R.id.container,
+                        LoginFragment()
+                )
+                .addToBackStack(LoginFragment.TAG)
+                .commit()
     }
 }
---
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.joaoricardi.bankapp.feature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.joaoricardi.bankapp.R
+
+class LoginFragment : Fragment() {
+
+    companion object{
+        val TAG = "login_fragment"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
