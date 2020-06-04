9ad665d07e61308413615f03dfd374505c6546f2
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-04 13:48:56 -0300

    colocando o clic no btn de sair e settando os dados de useraccount na tela

--- BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/home/HomeFragment.kt
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/home/HomeFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 
 import com.joaoricardi.bankapp.R
 import com.joaoricardi.bankapp.models.login.UserAccont
+import kotlinx.android.synthetic.main.home_card_info.*
 
 
 class HomeFragment : Fragment() {
@@ -21,6 +23,19 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logoutImageId.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStack(TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+
+        arguments?.getParcelable<UserAccont>(USER_ACCOUONT)?.let {
+            userNameTextId.text = it.name
+            userContaTextId.text = resources.getString(R.string.bankAccount, it.bankAccount, it.agency)
+            userSaldoTextId.text = resources.getString(R.string.moneyValue, it.balance)
+        }
     }
 
     companion object {
