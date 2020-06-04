db1370fd7ab8432aef1421557978a1a516771bf9
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-04 12:47:30 -0300

    layout tela de login com styles e os drawbles que serao usados

--- BankApp/app/src/main/java/com/joaoricardi/bankapp/MainActivity.kt
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpFragment()
     }
 
     private fun setUpFragment(){
