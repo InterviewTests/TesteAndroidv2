8f5a7ce666d0bed7c337b7b34953279613bd7198
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-04 12:59:38 -0300

    criacao das instancia dos retrofit sem injecao de dependeica devido ao tamanho do projeto

--- BankApp/app/src/main/java/com/joaoricardi/bankapp/service/RetrofitService.kt
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/service/RetrofitService.kt
@@ -1,2 +1,28 @@
 package com.joaoricardi.bankapp.service
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.joaoricardi.bankapp.service.api.LoginApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RetrofitService {
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+    companion object {
+        val BASE_URL = "https://bank-app-test.herokuapp.com/api/"
+    }
+
+    fun getLoginApi(): LoginApi = retrofit.create(LoginApi::class.java)
+
+}
