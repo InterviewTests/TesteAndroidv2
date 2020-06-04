6ab8852c8711f52ae71a0605fa4519b529d33694
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-04 13:15:33 -0300

    settando o click no btn para chamar o login via viewmodel e settando staes para ir atualizando a ui de acordo com o status da requisicao

--- BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/login/LoginFragment.kt
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/login/LoginFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 import com.joaoricardi.bankapp.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
@@ -14,9 +16,56 @@ class LoginFragment : Fragment() {
         val TAG = "login_fragment"
     }
 
+    private val viewModel: LoginViewModel by lazy {
+        ViewModelProviders.of(this).get(LoginViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpStates()
+        setBtnClick()
+    }
 
+    private fun setUpStates(){
+        viewModel.state.observeForever {state ->
+            when(state){
+                is LoginViewModel.ScreenState.Initial -> {
+                    loginButton.visibility = View.VISIBLE
+                    loadingProgressId.visibility  = View.GONE
+                    errorFeedBackId.visibility = View.GONE
+                }
+
+                is LoginViewModel.ScreenState.Loading -> {
+                    loginButton.visibility = View.GONE
+                    loadingProgressId.visibility  = View.VISIBLE
+                    errorFeedBackId.visibility = View.GONE
+                }
+
+                is LoginViewModel.ScreenState.Error -> {
+                    loginButton.visibility = View.GONE
+                    loadingProgressId.visibility  = View.GONE
+                    errorFeedBackId.visibility = View.VISIBLE
+
+                }
+            }
+        }
+
+    }
+
+    private fun setBtnClick(){
+        loginButton.setOnClickListener {
+            val email = emailEditId.editableText.toString()
+            val password = passwordEditId.editableText.toString()
+            viewModel.login(
+                email,password
+            )
+        }
+    }
+
+    override fun onResume() {
+        emailEditId.editableText.clear()
+        passwordEditId.editableText.clear()
+        super.onResume()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
