bc1e8654e7771fc428bb2e156dc67d068e6f3b27
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-06-04 11:36:43 -0300

    StatementsActivity criada

---
+++ app/src/main/java/com/gft/testegft/statements/StatementsActivity.java
@@ -0,0 +1,16 @@
+package com.gft.testegft.statements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gft.testegft.R;
+
+public class StatementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statementes);
+    }
+}
