6d6ddba4a1c5167c2400e785c45e2beec815ce8d
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-04 13:04:54 -0300

    criacao de variaveis para a chamada e apos a mesma ser realizada , criacao das variasves para usar o coroutines e criacao de States esperador pela UI

--- BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/login/LoginViewModel.kt
+++ BankApp/app/src/main/java/com/joaoricardi/bankapp/feature/login/LoginViewModel.kt
@@ -1,7 +1,33 @@
 package com.joaoricardi.bankapp.feature.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.joaoricardi.bankapp.models.login.UserAccont
+import com.joaoricardi.bankapp.service.repositoy.login.LoginRepository
+import com.joaoricardi.bankapp.service.repositoy.login.LoginRepositoryContract
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class LoginViewModel : ViewModel(){
+    private val loginRepository: LoginRepositoryContract = LoginRepository()
 
+    private val _userAccont = MutableLiveData<UserAccont>()
+    val userAccont: LiveData<UserAccont>
+        get() = _userAccont
+
+    private val _state = MutableLiveData<ScreenState>()
+    val state: LiveData<ScreenState>
+        get() = _state
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
+
+
+    sealed class ScreenState {
+        object Loading: ScreenState()
+        object Initial: ScreenState()
+        object Error: ScreenState()
+    }
 }
