5f1abec4ab1fa169d884021deb88b9e8a7915866
kevin <kevin.marcelo@gold360.com.br>
2020-06-10 16:09:49 -0300

    initial commit

---
+++ Project/app/src/main/java/projects/kevin/bankapp/User/Detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package projects.kevin.bankapp.User.Detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projects.kevin.bankapp.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ Project/app/src/main/java/projects/kevin/bankapp/User/Login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package projects.kevin.bankapp.User.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projects.kevin.bankapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
