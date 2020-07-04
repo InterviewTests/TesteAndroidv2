7e500e11f077fcffd889238b6eb129004ef7ddad
Marlon Rocha <marlon.rocha@aisdigital.com.br>
2020-07-04 18:11:19 -0300

    Criados novos pacotes para adicionar suas respectivas classes e responsabilidades.

--- TesteAndroid/app/src/main/java/br/com/mdr/testeandroid/flow/main/MainActivity.kt
+++ TesteAndroid/app/src/main/java/br/com/mdr/testeandroid/flow/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.mdr.testeandroid
+package br.com.mdr.testeandroid.flow.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.mdr.testeandroid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
