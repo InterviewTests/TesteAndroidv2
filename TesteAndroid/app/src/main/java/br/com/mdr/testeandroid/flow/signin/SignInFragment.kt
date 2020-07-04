aece8df9da427e3856e98081889b0c0e77180e40
Marlon Rocha <marlon.rocha@aisdigital.com.br>
2020-07-04 18:20:25 -0300

    Criado fragment de Sign In e adicionado ao gráfico de navegação, responsável pelo gerenciamento da navegação entre os Fragments, utilizando Navigation Architecture, que faz parte do Android Jetpack.

---
+++ TesteAndroid/app/src/main/java/br/com/mdr/testeandroid/flow/signin/SignInFragment.kt
@@ -0,0 +1,32 @@
+package br.com.mdr.testeandroid.flow.signin
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.mdr.testeandroid.R
+
+class SignInFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignInFragment()
+    }
+
+    private lateinit var viewModel: SignInViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.sign_in_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SignInViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ TesteAndroid/app/src/main/java/br/com/mdr/testeandroid/flow/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.mdr.testeandroid.flow.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
