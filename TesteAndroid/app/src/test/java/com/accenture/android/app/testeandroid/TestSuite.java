5be432958cc307821ee863e38fdf63c021a56169
Denis Magno <drmagno@icotel.net>
2020-07-10 17:48:40 -0300

    Inclusão do runner de todos os testes do sistema

---
+++ TesteAndroid/app/src/test/java/com/accenture/android/app/testeandroid/TestSuite.java
@@ -0,0 +1,16 @@
+package com.accenture.android.app.testeandroid;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Denis Magno on 10/07/2020.
+ * denis_magno16@hotmail.com
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        FieldHelperUnitTest.class,
+        FormatHelperUnitTest.class,
+})
+public class TestSuite {
+}
