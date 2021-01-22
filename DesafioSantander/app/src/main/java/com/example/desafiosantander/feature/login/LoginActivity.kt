cdfa176e3dbf32b83f0c55cbcbdabe4ed8e78a09
Maherbson Dantas <maherbson.dantas@picpay.com>
2021-01-22 12:09:52 -0300

    test

--- DesafioSantander/app/src/main/java/com/example/desafiosantander/feature/login/LoginActivity.kt
+++ DesafioSantander/app/src/main/java/com/example/desafiosantander/feature/login/LoginActivity.kt
@@ -25,6 +25,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         observeLiveData()
