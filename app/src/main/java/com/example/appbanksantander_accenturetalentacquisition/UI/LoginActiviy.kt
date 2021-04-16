4b7957707b0cf8569ce9cdfbaec64196eba8f30f
Henrique Teixeira <ht.henrique@live.com>
2021-04-16 13:46:54 -0300

    Adicionada a tela de login e a tela principal para a mesma pasta de UI

---
+++ app/src/main/java/com/example/appbanksantander_accenturetalentacquisition/UI/LoginActiviy.kt
@@ -0,0 +1,12 @@
+package com.example.appbanksantander_accenturetalentacquisition.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appbanksantander_accenturetalentacquisition.R
+
+class LoginActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
