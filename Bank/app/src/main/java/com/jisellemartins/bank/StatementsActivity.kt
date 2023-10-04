d6c6692fa2c559e38f89d8150a5e3da078f1af6c
Jiselle Martins <jisellevms@gmail.com>
2023-10-04 18:30:25 -0300

    finalizando construcao de layouts/telas

--- Bank/app/src/main/java/com/jisellemartins/bank/StatementsActivity.kt
+++ Bank/app/src/main/java/com/jisellemartins/bank/StatementsActivity.kt
@@ -3,9 +3,9 @@ package com.jisellemartins.bank
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StatementsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_statements)
     }
 }
